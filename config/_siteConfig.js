afd3b4593b6524426b0952344d8ef985b35113ef
writerben <64561235+writerben@users.noreply.github.com>
2020-08-07 15:26:46 +0100

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -10,7 +10,7 @@ import theCMS from '../cms/netlify/cms'
  */
 export default {
   siteName: 'Vacancypool',
-  tagline: 'Employement Tips',
+  tagline: 'Blogs on career. A pool of information',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
