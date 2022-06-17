a21074868cd0e793de436b12eda806d7c8476714
writerben <64561235+writerben@users.noreply.github.com>
2022-06-17 20:31:31 +0100

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
-      title="VAbout Us"
+      title="About Us"
       subtitle="History of Vacancypool"
       image="/uploads/about-hero.jpg"
     ></site-hero>
