c0bc3fcb6772253a1d350e5aaa06ffbb1531e2ae
writerben <64561235+writerben@users.noreply.github.com>
2022-06-22 11:39:10 +0100

    Create JobCardList.vue

---
+++ components/JobCardList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="grid gap-x-12 gap-y-12 grid-cols-1 sm:grid-cols-3 container mx-auto px-5 sm:gap-y-20"
+    style="max-width: 1100px"
+  >
+    <job-card-list-item
+      v-for="job in jobs"
+      :key="job.id"
+      :id="job.id"
+      :name="job.companyName"
+      :title="job.title"
+      :jobType="job.jobType"
+      :location="job.location"
+      :logo="job.companyLogo"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    jobs: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
